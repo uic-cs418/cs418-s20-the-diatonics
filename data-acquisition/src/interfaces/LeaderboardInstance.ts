c7460ce67af99c0fcf87986c76d2cecb5720cb2f
Faraz <Fshakir>
2020-03-05 21:12:44 -0600

    Removing from Faraz

--- data-acquisition/src/interfaces/LeaderboardInstance.ts
+++
@@ -1,9 +0,0 @@
-import moment = require('moment');
-
-export default interface LeaderboardInstance {
-    spotifyId: string
-    weekOf: moment.Moment
-    position: number
-    changeSinceLastWeek: number
-    weeksOnChart: number
-}
--- data-acquisition/src/interfaces/Track.ts
+++
@@ -1,9 +0,0 @@
-export default interface Track {
-    spotifyId: string
-    title: string
-    artist: string
-    // duration: number
-    // danceability: number
-    // energy: number
-    // instrumentalness: number
-}

0594abdbdf5876413c4ac292ae24ac883589462d
Mateusz Rusnaczyk <mrusnaczyk@gmail.com>
2020-04-22 16:47:52 -0500

    more spotify cleanup

--- lib/data_acquisition_cleaning.py
+++ lib/data_acquisition_cleaning.py
@@ -5,13 +5,7 @@ def load_song_data():
 
 # returns a list of strings containing song titles and artists to be queried
 def get_title_artist(song_data):
-    # extracting title and names of the artists/bands
-    titles = song_data['Song Clean'].tolist()
-    artists = song_data['ARTIST CLEAN'].tolist()
-
-    # creating a list of titles+aritsts to be queried
-    ta_list = []
-    for i in range(0, len(titles)):
-        ta_list.append(titles[i] + " " + artists[i])
-
-    return ta_list
+    return list(zip(
+        song_data['Song Clean'], 
+        song_data['ARTIST CLEAN']
+    ))
--- lib/spotify.py
+++ lib/spotify.py
@@ -7,6 +7,7 @@ def get_spotify_client():
     '''
     Returns an instantiated Spotify API client, using the client id and secret provided in the files ClientID.txt and ClientSecret.txt
     '''
+
     def read_api_key(filepath):
         with open(filepath, 'r') as f:
             return f.read().replace('\n','')
@@ -18,53 +19,66 @@ def get_spotify_client():
 
     return spotipy.Spotify(client_credentials_manager = credentials)
 
-def get_spotify_resource_url(title_artist, sp):
+def get_spotify_resource_urls(title_artist, sp):
     '''
     Queries the Spotify API to retrieve the Spotify resource URL corresponding to a given track
-    Input: List containing titles and artists to search for
+    Input: List of tuples containing titles and artists to search for
     Output: List containing the Spotify resource URLS corresponding to each track
     '''
+
     url_list = []
-    for song in title_artist:
+    for track_name, artist_name in title_artist:
         try:
             url_list.append(
-                sp.search(
-                    q=song, type='track', limit=1
-                )['tracks']['items'][0]['external_urls']['spotify']
+                (
+                    track_name,
+                    artist_name,
+                    sp.search(
+                        q=track_name, type='track', limit=1
+                    )['tracks']['items'][0]['external_urls']['spotify']
+                )
             )
         except:
             None
           # print(song)
     return url_list
 
-def get_audio_features(urls, sp):
+def get_audio_features(tracks_to_query, sp):
     '''
     Retrieves the Spotify audio features for a collection of urls.
     Input: List of Spotify song urls
     Output: Dataframe containing the audio features for each track
     '''
+    urls = [track[2] for track in tracks_to_query]
     audio_features_dict = sp.audio_features(tracks=urls)
+
     df = pd.DataFrame.from_dict(audio_features_dict)
     df = df.drop(columns=['time_signature', 'duration_ms', 'mode', 'type', 'id', 'uri','track_href','analysis_url'])
+
+    track_names = [track[0] for track in tracks_to_query]
+    artist_names = [track[1] for track in tracks_to_query]
+    df['title'] = track_names
+    df['artist'] = artist_names
+
     return df
 
 
-def audio_features(urls, sp):
+def audio_features(tracks_to_query, sp):
     '''
     A wrapper for `get_audio_features()`, which gets the audio features of 100 songs 
     at a time, and then returns the combined results.
-    Input: List of Spotify song urls
+    Input: List of tuples containing track names, artist names, and Spotify song urls
     Output: Dataframe containing the audio features for each track
     '''
     j=0
     dataframe = pd.DataFrame()
     while(True):
-        if j+100 < len(urls):
-            df = get_audio_features(urls[j:j+100], sp)
+        if j+100 < len(tracks_to_query):
+            df = get_audio_features(tracks_to_query[j:j+100], sp)
             dataframe = dataframe.append(df)
             j = j+100
         else:
-            df = get_audio_features(urls[j:len(urls)], sp)
+            df = get_audio_features(tracks_to_query[j:len(tracks_to_query)], sp)
             dataframe = dataframe.append(df)
             break
     dataframe.reset_index(inplace = True)
